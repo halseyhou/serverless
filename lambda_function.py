b3a483f30168642c09166b15442c6559e33cf687
Cici Hou <41445818+cicihou@users.noreply.github.com>
2022-11-18 20:09:57 -0800

    add: a8

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,12 @@
-import json
 import boto3
+import json
 from botocore.exceptions import ClientError
 
 
 def lambda_handler(event, context):
     payloads = event['Records'][0]["Sns"]
     msg = json.loads(payloads['Message'])
-    SENDER = "notification@demo.xihou.me"
+    SENDER = "support@demo.xihou.me"
     RECIPIENT = msg['to']
 
     DESTINATION = {'ToAddresses': [
@@ -19,8 +19,15 @@ def lambda_handler(event, context):
     CHARSET = "UTF-8"
 
     client = boto3.client('ses')
+    dynamo = boto3.resource('dynamodb')
+    table = dynamo.Table('emailTrackingTable')
 
     try:
+        resp = table.get_item(Key={'email': BODY})
+        item = resp.get('Item', {})
+        if item:
+            print('The email content has been sent!')
+            return {'statusCode': 200, 'body': json.dumps(item)}
         resp = client.send_email(
             Destination=DESTINATION,
             Message={
@@ -37,10 +44,13 @@ def lambda_handler(event, context):
             },
             Source=SENDER,
         )
+        table.put_item(Item={'email': BODY})
+
     # Display an error if something goes wrong.
     except ClientError as e:
         print(e.response['Error']['Message'])
     else:
+        print(resp)
         print("Email sent! Message ID:")
         print(resp['MessageId'])
         print(RECIPIENT)
